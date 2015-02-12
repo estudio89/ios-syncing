fa4248c9871b96d82e231ff01880ac32e84df842
Rodrigo Suhr <rosuhr@gmail.com>
2015-02-12 18:01:16 -0200

    ServerComm

---
+++ Syncing/ServerComm.h
@@ -0,0 +1,17 @@
+//
+//  ServerComm.h
+//  Syncing
+//
+//  Created by Rodrigo Suhr on 2/12/15.
+//  Copyright (c) 2015 Estúdio 89 Desenvolvimento de Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ServerComm : NSObject
+
++ (id)getInstance;
+- (NSDictionary *)post:(NSString *)url withData:(NSDictionary *)data;
+- (NSDictionary *)post:(NSString *)url withData:(NSDictionary *)data withFiles:(NSArray *)files;
+
+@end
---
+++ Syncing/ServerComm.m
@@ -0,0 +1,28 @@
+//
+//  ServerComm.m
+//  Syncing
+//
+//  Created by Rodrigo Suhr on 2/12/15.
+//  Copyright (c) 2015 Estúdio 89 Desenvolvimento de Software. All rights reserved.
+//
+
+#import "ServerComm.h"
+
+@implementation ServerComm
+
++ (id)getInstance
+{
+    return [self class];
+}
+
+- (NSDictionary *)post:(NSString *)url withData:(NSDictionary *)data
+{
+    return [self post:url withData:data withFiles:nil];
+}
+
+- (NSDictionary *)post:(NSString *)url withData:(NSDictionary *)data withFiles:(NSArray *)files
+{
+    
+}
+
+@end
