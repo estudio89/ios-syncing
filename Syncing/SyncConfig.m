2da6ac5b19472a3b2c137441c6b5e3582d08a50b
Rodrigo Suhr <rosuhr@gmail.com>
2015-02-27 18:02:29 -0300

    SyncConfig

--- Syncing/SyncConfig.m
+++ Syncing/SyncConfig.m
@@ -8,7 +8,57 @@
 
 #import "SyncConfig.h"
 
+@interface SyncConfig()
+
+@property (nonatomic, strong, readwrite) NSString *configFile;
+@property (nonatomic, strong, readwrite) NSMutableDictionary *syncManagersByIdentifier;
+@property (nonatomic, strong, readwrite) NSMutableDictionary *syncManagersByResponseIdentifier;
+@property (nonatomic, strong, readwrite) NSString *mGetDataUrl;
+@property (nonatomic, strong, readwrite) NSString *mSendDataUrl;
+@property (nonatomic, strong, readwrite) NSString *mAuthenticateUrl;
+@property (nonatomic, strong, readwrite) NSMutableDictionary *mModelGetDataUrls;
+
+@end
+
 @implementation SyncConfig
+
+/**
+ init
+ */
+- (id)init
+{
+    if (self = [super init])
+    {
+        self.syncManagersByIdentifier = [[NSMutableDictionary alloc] init];
+        self.syncManagersByResponseIdentifier = [[NSMutableDictionary alloc] init];
+        self.mModelGetDataUrls = [[NSMutableDictionary alloc] init];
+    }
+    
+    return self;
+}
+
+/**
+ setConfigFile
+ */
+- (void)setConfigFile:(NSString *)filename
+{
+    self.configFile = filename;
+    [self loadSettings];
+    [self setupSyncing];
+}
+
+/**
+ loadSettings
+ */
+- (void)loadSettings
+{
+    
+}
+
+- (void)setupSyncing
+{
+    
+}
 
 - (NSString *)getAuthToken
 {
@@ -20,14 +70,27 @@
     return @"";
 }
 
+/**
+ getGetDataUrl
+ */
 - (NSString *)getGetDataUrl
 {
-    return @"";
+    return self.mGetDataUrl;
 }
 
+/**
+ getGetDataUrlForModel
+ */
 - (NSString *)getGetDataUrlForModel:(NSString *)identifier
 {
-    return @"";
+    NSString *url = [self.mModelGetDataUrls valueForKey:identifier];
+    
+    if (url == nil)
+    {
+        @throw([NSException exceptionWithName:@"URL not found" reason:@"URL not found for the identifier." userInfo:nil]);
+    }
+    
+    return url;
 }
 
 - (NSString *)getDeviceId
@@ -35,30 +98,46 @@
     return @"";
 }
 
+/**
+ getSyncManagers
+ */
 - (NSArray *)getSyncManagers
 {
-    return [[NSArray alloc] init];
+    return [self.syncManagersByIdentifier allValues];
 }
 
+/**
+ getSendDataUrl
+ */
 - (NSString *)getSendDataUrl
 {
-    return @"";
+    return self.mSendDataUrl;
 }
 
 - (void)setTimestamp:(NSString *)timestamp
 {
     
 }
+
+/**
+ getSyncManagerByResponseId
+ */
 - (id<SyncManager>)getSyncManagerByResponseId:(NSString *)responseId
 {
-    return nil;
+    return [self.syncManagersByResponseIdentifier objectForKey:responseId];
 }
 
+/**
+ getSyncManager
+ */
 - (id<SyncManager>)getSyncManager:(NSString *)identifier
 {
-    return nil;
+    return [self.syncManagersByIdentifier objectForKey:identifier];
 }
 
+/**
+ getDatabase
+ */
 - (DatabaseProvider *)getDatabase
 {
     return [[DatabaseProvider alloc] init];
