151801e6821297602b0937410becaae29615d1ed
Rodrigo Suhr <rosuhr@gmail.com>
2015-03-09 17:28:29 -0300

    DataSyncHelperTests

--- Syncing Tests/DataSyncHelperTests.m
+++ Syncing Tests/DataSyncHelperTests.m
@@ -10,13 +10,24 @@
 #import <XCTest/XCTest.h>
 #import <OCMock/OCMock.h>
 #import "SyncManager.h"
+#import "SyncConfig.h"
+#import "DataSyncHelper.h"
+#import "CustomTransactionManager.h"
+#import "ThreadChecker.h"
+#import "AsyncBus.h"
 
 @interface DataSyncHelperTests : XCTestCase
 
 @property (nonatomic) id syncManagerRegistros;
 @property (nonatomic) id syncManagerEmpresas;
 @property (nonatomic) id syncManagerFormularios;
+@property (nonatomic) id syncConfig;
+@property (nonatomic) id serverComm;
+@property (nonatomic) id bus;
 @property (nonatomic) NSMutableArray *modifiedFiles;
+@property (nonatomic) DataSyncHelper *dataSyncHelper;
+@property (nonatomic) CustomTransactionManager *customTransactionManager;
+@property (nonatomic) ThreadChecker *threadChecker;
 
 @end
 
@@ -63,6 +74,76 @@
     OCMStub([_syncManagerEmpresas getModifiedData]).andReturn(empresasModified);
     OCMStub([_syncManagerEmpresas shouldSendSingleObject]).andReturn(NO);
     OCMStub([_syncManagerEmpresas getModifiedFiles]).andReturn([[NSMutableArray alloc] init]);
+    OCMStub([_syncManagerEmpresas getResponseIdentifier]).andReturn(@"empresas_id");
+    OCMStub([_syncManagerEmpresas hasModifiedData]).andReturn(YES);
+    
+    // Formularios
+    OCMStub([_syncManagerFormularios getIdentifier]).andReturn(@"formularios");
+    OCMStub([_syncManagerFormularios saveNewData:[OCMArg any] withDeviceId:[OCMArg any]]).andReturn([[NSMutableArray alloc] init]);
+    OCMStub([_syncManagerFormularios getModifiedData]).andReturn([[NSMutableArray alloc] init]);
+    OCMStub([_syncManagerFormularios shouldSendSingleObject]).andReturn(NO);
+    OCMStub([_syncManagerFormularios getModifiedFiles]).andReturn([[NSMutableArray alloc] init]);
+    OCMStub([_syncManagerFormularios getResponseIdentifier]).andReturn(@"formularios_id");
+    OCMStub([_syncManagerFormularios hasModifiedData]).andReturn(NO);
+    
+    // SyncConfig
+    _syncConfig = OCMClassMock([SyncConfig class]);
+    OCMStub([_syncConfig getAuthToken]).andReturn(@"123");
+    OCMStub([_syncConfig getTimestamp]).andReturn(@"666");
+    //OCMStub([_syncConfig getDatabase]).andReturn(@"");
+    OCMStub([_syncConfig getGetDataUrl]).andReturn(@"http://127.0.0.1:8000/api/get-data/");
+    OCMStub([_syncConfig getSendDataUrl]).andReturn(@"http://127.0.0.1:8000/api/send-data/");
+    OCMStub([_syncConfig getDeviceId]).andReturn(@"asdasda");
+    NSMutableArray *syncManagers = [[NSMutableArray alloc] init];
+    [syncManagers addObject:_syncManagerRegistros];
+    [syncManagers addObject:_syncManagerEmpresas];
+    [syncManagers addObject:_syncManagerFormularios];
+    OCMStub([_syncConfig getSyncManagers]).andReturn(syncManagers);
+    OCMStub([_syncConfig getSyncManager:@"registros"]).andReturn(_syncManagerRegistros);
+    OCMStub([_syncConfig getSyncManager:@"empresas"]).andReturn(_syncManagerEmpresas);
+    OCMStub([_syncConfig getSyncManager:@"formularios"]).andReturn(_syncManagerFormularios);
+    OCMStub([_syncConfig getSyncManagerByResponseId:@"registros_id"]).andReturn(_syncManagerRegistros);
+    OCMStub([_syncConfig getSyncManagerByResponseId:@"empresas_id"]).andReturn(_syncManagerEmpresas);
+    OCMStub([_syncConfig getSyncManagerByResponseId:@"formularios_id"]).andReturn(_syncManagerFormularios);
+    
+    OCMStub([_syncConfig getGetDataUrlForModel:@"registros"]).andReturn(@"http://127.0.0.1:8000/api/get-data/registros/");
+    
+    // CustomTransactionManager
+    _customTransactionManager = [[CustomTransactionManager alloc] init];
+    
+    // ServerComm
+    NSString *jsonFile = @"/Users/rodrigosuhr/Dev/ios-syncing/Syncing\ Tests/get-data-response.json";
+    NSString *jsonResponse = [[NSString alloc] initWithContentsOfFile:jsonFile encoding:NSUTF8StringEncoding error:nil];
+    NSData *dataJsonResponse = [jsonResponse dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
+    NSDictionary *jsonGetResponse = [NSJSONSerialization JSONObjectWithData:dataJsonResponse options:kNilOptions error:nil];
+    
+    jsonFile = @"/Users/rodrigosuhr/Dev/ios-syncing/Syncing\ Tests/send-data-response.json";
+    jsonResponse = [[NSString alloc] initWithContentsOfFile:jsonFile encoding:NSUTF8StringEncoding error:nil];
+    dataJsonResponse = [jsonResponse dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
+    NSDictionary *jsonSendResponse = [NSJSONSerialization JSONObjectWithData:dataJsonResponse options:kNilOptions error:nil];
+    
+    jsonFile = @"/Users/rodrigosuhr/Dev/ios-syncing/Syncing\ Tests/get-data-for-model-response.json";
+    jsonResponse = [[NSString alloc] initWithContentsOfFile:jsonFile encoding:NSUTF8StringEncoding error:nil];
+    dataJsonResponse = [jsonResponse dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
+    NSDictionary *jsonGetResponseForModel = [NSJSONSerialization JSONObjectWithData:dataJsonResponse options:kNilOptions error:nil];
+    
+    _serverComm = OCMClassMock([ServerComm class]);
+    OCMStub([_serverComm post:[_syncConfig getGetDataUrl] withData:[OCMArg any]]).andReturn(jsonGetResponse);
+    OCMStub([_serverComm post:[_syncConfig getSendDataUrl] withData:[OCMArg any] withFiles:[OCMArg any]]).andReturn(jsonSendResponse);
+    OCMStub([_serverComm post:[_syncConfig getGetDataUrlForModel:@"registros"] withData:[OCMArg any]]).andReturn(jsonGetResponseForModel);
+    
+    // ThreadChecker
+    _threadChecker = [[ThreadChecker alloc] init];
+    
+    // AsyncBus
+    _bus = OCMClassMock([AsyncBus class]);
+    
+    // DataSyncHelper
+    _dataSyncHelper = [[DataSyncHelper alloc] initWithServer:_serverComm
+                                           withThreadChecker:_threadChecker
+                                              withSyncConfig:_syncConfig
+                                      withTransactionManager:_customTransactionManager
+                                                     withBus:_bus];
 }
 
 - (void)tearDown {
@@ -80,6 +161,30 @@
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
     }];
+}
+
+/**
+ testGetDataFromServer
+ */
+- (void)testGetDataFromServer
+{
+    NSString *jsonFile = @"/Users/rodrigosuhr/Dev/ios-syncing/Syncing\ Tests/get-data-request.json";
+    NSString *json = [[NSString alloc] initWithContentsOfFile:jsonFile encoding:NSUTF8StringEncoding error:nil];
+    NSData *dataRequest = [json dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
+    NSDictionary *jsonDataRequest = [NSJSONSerialization JSONObjectWithData:dataRequest options:kNilOptions error:nil];
+    
+    jsonFile = @"/Users/rodrigosuhr/Dev/ios-syncing/Syncing\ Tests/get-data-response.json";
+    json = [[NSString alloc] initWithContentsOfFile:jsonFile encoding:NSUTF8StringEncoding error:nil];
+    NSData *dataResponse = [json dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
+    
+    NSDictionary *data = @{@"token":[jsonDataRequest valueForKey:@"token"],
+                           @"timestamp":[jsonDataRequest valueForKey:@"timestamp"]};
+    
+    BOOL completed = [_dataSyncHelper getDataFromServer];
+    
+    NSDictionary *dd = @{@"tosken":@"1sd23", @"timestsamp":@"66sd60", @"sdsds":@"sdfsdf"};
+    OCMVerify([_serverComm post:@"http://127.0.0.1:8000/api/get-data/" withData:dd]);
+    //OCMVerify([_serverComm post:@"http://127.0.0.1:8000/api/get-data/" withData:[OCMArg any]]);
 }
 
 @end
