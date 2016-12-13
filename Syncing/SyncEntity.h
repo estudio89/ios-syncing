604426aebcf909141afff266285513de65319273
Rodrigo Suhr <rosuhr@gmail.com>
2016-12-13 12:10:48 -0200

    Creating some wrapper methods to method getOldestFromEntity.

--- Syncing/SyncEntity.h
+++ Syncing/SyncEntity.h
@@ -16,6 +16,8 @@
 
 + (SyncEntity *)getOldestFromEntity:(NSString *)entity withContext:(NSManagedObjectContext *)context;
 + (SyncEntity *)getOldestFromEntity:(NSString *)entity withPredicate:(NSPredicate *)predicate withContext:(NSManagedObjectContext *)context;
++ (SyncEntity *)getOldestFromEntity:(NSString *)entity withKey:(NSString *)key withContext:(NSManagedObjectContext *)context;
++ (SyncEntity *)getOldestFromEntity:(NSString *)entity withPredicate:(NSPredicate *)predicate withKey:(NSString *)key withContext:(NSManagedObjectContext *)context;
 + (NSUInteger)countFromEntity:(NSString *)entity withContext:(NSManagedObjectContext *)context;
 + (NSUInteger)countFromEntity:(NSString *)entity withPredicate:(NSPredicate *)predicate withContext:(NSManagedObjectContext *)context;
 - (BOOL)equals:(NSManagedObject *)object withContext:(NSManagedObjectContext *)context;
